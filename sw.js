a389f0dbda63d51e381c81f7dcfdc2467609fa6d
solar262 <solardynamics592@gmail.com>
2026-06-02 13:59:16 +0200

    Bust service worker cache for arcade mode release

--- sw.js
+++ sw.js
@@ -1,8 +1,42 @@
-const CACHE_NAME = 'crime-empire-v1';
-const ASSETS = ['./','./index.html','./style.css','./game.js','./balance.js','./factions.js','./manifest.webmanifest'];
+const CACHE_NAME = 'crime-empire-v4-arcade';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './premium.css',
+  './arcade.css',
+  './game.js',
+  './balance.js',
+  './factions.js',
+  './meta.js',
+  './arcade.js',
+  './manifest.webmanifest'
+];
+
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
